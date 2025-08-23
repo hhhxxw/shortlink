f515b6fb3817b692b1191227be33bf05b958b19a
一只咸鱼的大厂梦 <3025731614@qq.com>
2025-08-23 23:07:59 +0800

    项目æ及其子模块的创建

---
+++ admin/src/main/java/com/nageoffer/shorlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,15 @@
+package com.nageoffer.shorlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 一只咸鱼的大厂梦-hxw
+ * @date 2025-08-23 22:34
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
