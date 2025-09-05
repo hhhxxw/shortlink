43576199ae5de6195dbe36b35998bd9b25649fbe
一只咸鱼的大厂梦 <3025731614@qq.com>
2025-09-05 11:12:03 +0800

    feature：修改配置文件MyBatis-Plus 进行数据库操作更新记录时，会自动调用 updateFill 方法更新时间字段(要注意类型匹配)

--- admin/src/main/java/com/nageoffer/shorlink/admin/config/MyMetaObjectHandler.java
+++ admin/src/main/java/com/nageoffer/shorlink/admin/config/MyMetaObjectHandler.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -24,6 +23,6 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.strictInsertFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
     }
 }
