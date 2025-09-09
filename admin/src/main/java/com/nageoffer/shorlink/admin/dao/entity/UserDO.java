b96ab3a6b333826e8bcadd2903dcba30c313f1d0
一只咸鱼的大厂梦 <3025731614@qq.com>
2025-09-09 10:38:30 +0800

    optimize: 优化用户持久层实体基础属性抽象

--- admin/src/main/java/com/nageoffer/shorlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/nageoffer/shorlink/admin/dao/entity/UserDO.java
@@ -1,10 +1,12 @@
 package com.nageoffer.shorlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.nageoffer.shorlink.admin.common.database.BaseDO;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  * @description t_user
@@ -13,7 +15,7 @@ import java.util.Date;
  */
 @TableName("t_user")
 @Data
-public class UserDO implements Serializable {
+public class UserDO extends BaseDO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -53,25 +55,6 @@ public class UserDO implements Serializable {
      */
 
     private Long deletionTime;
-
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Integer delFlag;
-
 
     public UserDO() {}
 }
