35e1230c00f026624db75808dc5d782572716d3b
一只咸鱼的大厂梦 <3025731614@qq.com>
2025-09-11 10:02:07 +0800

    feature： 开发短链接后管系统用户登陆时间修改为30天

--- admin/src/main/java/com/nageoffer/shorlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shorlink/admin/service/impl/UserServiceImpl.java
@@ -124,7 +124,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         String uuid = UUID.randomUUID().toString();
         stringRedisTemplate.opsForHash().put("login_" + requestParam.getUsername(), uuid, JSON.toJSONString(userDO));
         // 设置过期时间
-        stringRedisTemplate.expire("login_" + requestParam.getUsername(), 30L, TimeUnit.MINUTES);
+        stringRedisTemplate.expire("login_" + requestParam.getUsername(), 30L, TimeUnit.DAYS);
         return new UserLoginRespDTO(uuid);
     }
 
