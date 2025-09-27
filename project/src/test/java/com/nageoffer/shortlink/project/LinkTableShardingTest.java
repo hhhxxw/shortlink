46497f04201a8a2e2d5f65d7782827e290f57d40
一只咸鱼的大厂梦 <3025731614@qq.com>
2025-09-27 20:46:08 +0800

    fixed/添加图标字段，重新分t_link表

--- project/src/test/java/com/nageoffer/shortlink/project/LinkTableShardingTest.java
+++ project/src/test/java/com/nageoffer/shortlink/project/LinkTableShardingTest.java
@@ -9,24 +9,27 @@ package com.nageoffer.shortlink.project;
  * @since 2025/9/22
  */
 public class LinkTableShardingTest {
-    public static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
-            "                          `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "                          `domain` varchar(128) DEFAULT NULL COMMENT '域名',\n" +
-            "                          `short_uri` varchar(8) DEFAULT NULL COMMENT '短链接',\n" +
-            "                          `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',\n" +
-            "                          `origin_url` varchar(1024) DEFAULT NULL COMMENT '原始链接',\n" +
-            "                          `click_num` int(11) DEFAULT 0 COMMENT '点击量',\n" +
-            "                          `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
-            "                          `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：未启用 1：已启用',\n" +
-            "                          `created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：控制台 1：接口',\n" +
-            "                          `valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：用户自定义',\n" +
-            "                          `valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
-            "                          `describe` varchar(1024) DEFAULT NULL COMMENT '描述',\n" +
-            "                          `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
-            "                          `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
-            "                          `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
-            "                          PRIMARY KEY (`id`),\n" +
-            "                          UNIQUE KEY `idx_unique_full_short_uri` (`full_short_url`) USING BTREE\n" +
+    public static final String SQL = "create table t_link_%d\n" +
+            "(\n" +
+            "    id              bigint auto_increment comment 'ID'\n" +
+            "        primary key,\n" +
+            "    domain          varchar(128)  null comment '域名',\n" +
+            "    short_uri       varchar(8)    null comment '短链接',\n" +
+            "    full_short_url  varchar(128)  null comment '完整短链接',\n" +
+            "    origin_url      varchar(1024) null comment '原始链接',\n" +
+            "    click_num       int default 0 null comment '点击量',\n" +
+            "    gid             varchar(32)   null comment '分组标识',\n" +
+            "    favicon         varchar(256)  null comment '网站图标',\n" +
+            "    enable_status   tinyint(1)    null comment '启用标识 0：未启用 1：已启用',\n" +
+            "    created_type    tinyint(1)    null comment '创建类型 0：控制台 1：接口',\n" +
+            "    valid_date_type tinyint(1)    null comment '有效期类型 0：永久有效 1：用户自定义',\n" +
+            "    valid_date      datetime      null comment '有效期',\n" +
+            "    `describe`      varchar(1024) null comment '描述',\n" +
+            "    create_time     datetime      null comment '创建时间',\n" +
+            "    update_time     datetime      null comment '修改时间',\n" +
+            "    del_flag        tinyint(1)    null comment '删除标识 0：未删除 1：已删除',\n" +
+            "    constraint idx_unique_full_short_uri\n" +
+            "        unique (full_short_url)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
