af0a9b0628066e701233c8d75e2586602b82609c
一只咸鱼的大厂梦 <3025731614@qq.com>
2025-10-02 17:39:59 +0800

    feature/短链接分组分表处理

--- admin/src/test/java/com/nageoffer/shorlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/nageoffer/shorlink/admin/test/UserTableShardingTest.java
@@ -6,21 +6,19 @@ package com.nageoffer.shorlink.admin.test;
  */
 public class UserTableShardingTest {
     public static final String SQL = "-- auto-generated definition\n" +
-            "create table t_user_%d\n" +
+            "create table t_group_%d\n" +
             "(\n" +
-            "    id            bigint auto_increment comment 'ID'\n" +
+            "    id          bigint auto_increment comment 'ID'\n" +
             "        primary key,\n" +
-            "    username      varchar(256) null comment '用户名',\n" +
-            "    password      varchar(512) null comment '密码',\n" +
-            "    real_name     varchar(256) null comment '真实姓名',\n" +
-            "    phone         varchar(128) null comment '手机号码',\n" +
-            "    mail          varchar(512) null comment '邮箱',\n" +
-            "    deletion_time bigint       null comment '注销时间戳',\n" +
-            "    create_time   datetime     null comment '创建时间',\n" +
-            "    update_time   datetime     null comment '修改时间',\n" +
-            "    del_flag      tinyint(1)   null comment '删除标识0：未删除 1：已删除',\n" +
-            "    constraint idx_unique_username\n" +
-            "        unique (username)\n" +
+            "    gid         varchar(32)  null comment '分组标识',\n" +
+            "    name        varchar(64)  null comment '分组名称',\n" +
+            "    username    varchar(256) null comment '创建分组用户名',\n" +
+            "    sort_order  int          null comment '分组排序',\n" +
+            "    create_time datetime     null comment '创建时间',\n" +
+            "    update_time datetime     null comment '修改时间',\n" +
+            "    del_flag    tinyint(1)   null comment '删除标识 0：未删除 1：已删除',\n" +
+            "    constraint idx_unique_username_gid\n" +
+            "        unique (gid, username)\n" +
             ");\n" +
             "\n";
     public static void main(String[] args) {
