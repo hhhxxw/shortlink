c878a84470f6720815bb3b44668ed577d8bffedf
一只咸鱼的大厂梦 <3025731614@qq.com>
2025-10-04 10:17:38 +0800

    feature: 前端传递有效期时间序列化

--- admin/src/main/java/com/nageoffer/shorlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
+++ admin/src/main/java/com/nageoffer/shorlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
@@ -1,5 +1,6 @@
 package com.nageoffer.shorlink.admin.remote.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -42,6 +43,7 @@ public class ShortLinkCreateReqDTO {
     /**
      * 有效期类型 0：永久有效 1：用户自定义
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Integer validDateType;
 
     /**
--- project/src/main/java/com/nageoffer/shorlink/project/dto/req/ShortLinkCreateReqDTO.java
+++ project/src/main/java/com/nageoffer/shorlink/project/dto/req/ShortLinkCreateReqDTO.java
@@ -1,5 +1,6 @@
 package com.nageoffer.shorlink.project.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -47,6 +48,7 @@ public class ShortLinkCreateReqDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**
